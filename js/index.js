db12f5fb35446d8f2462450315600d9078e0b889
Viktoriia <viktoriia.pavlus@gmail.com>
2019-10-02 16:00:10 +0000

    JS Sample

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const greetings= 'Hello JavaScript!';
+alert(greetings);
+
+let userName;
+userName = prompt('Enter your name');
+alert('Hello ' + userName);
+
+const userAge = parseFloat(prompt('Enter your age'));
+alert('Your age is ' + userAge);
+
+const nextAge =  addOne(userAge);
+alert('Next your you will be ' + nextAge);
+
+if (Number.isNan(userAge)) {
+    alert('Wrong age!');
+} else if (userAge  >= 18 ){
+    alert('You are adult!');
+} else {
+    alert('You are too young');
+}
+
+function addOne(input){
+    const result = input + 1;
+    return result;
+}
